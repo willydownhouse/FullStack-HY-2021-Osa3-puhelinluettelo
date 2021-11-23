aa06fece25db000d13011d3f6929a4036ca5ad4f
willydownhouse <ville.l.alatalo@gmail.com>
2021-11-23 11:19:13 +0100

    uibuild

---
+++ puhluetteloClient/src/components/AddNewPersonForm.js
@@ -0,0 +1,120 @@
+import React from "react";
+
+import phoneService from "../services/PhoneNumbers";
+
+const AddNewPersonForm = (props) => {
+  const {
+    phoneBook,
+    setPhoneBook,
+    newName,
+    setNewName,
+    phoneNumber,
+    setPhoneNumber,
+    setErrorMessage,
+    setSuccessMessage,
+  } = props;
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!phoneBook.map((person) => person.name).includes(newName)) {
+      phoneService
+        .create({ name: newName, number: phoneNumber })
+        .then((newPhoneNumber) => {
+          setPhoneBook(phoneBook.concat(newPhoneNumber.data));
+          setSuccessMessage("New number created succesfully!");
+
+          setTimeout(() => {
+            setSuccessMessage(null);
+          }, 5000);
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+          if (err.response.data.message) {
+            setErrorMessage(err.response.data.message);
+          } else {
+            setErrorMessage(
+              `Couldnt add ${newName} to the phonebook, please try again!`
+            );
+          }
+
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        });
+      setNewName("");
+      setPhoneNumber("");
+      return;
+    }
+
+    if (
+      window.confirm(
+        `${newName} is already in the phonebook, do you want to replace the old number with the new one`
+      )
+    ) {
+      const currentObj = phoneBook.find((ob) => ob.name === newName);
+
+      phoneService
+        .update(currentObj._id, {
+          name: newName,
+          number: phoneNumber,
+        })
+        .then((updatedObj) => {
+          setPhoneBook(
+            phoneBook.map((ob) =>
+              ob._id !== updatedObj.data._id ? ob : updatedObj.data
+            )
+          );
+
+          setSuccessMessage("Number updated succesfully!");
+
+          setTimeout(() => {
+            setSuccessMessage(null);
+          }, 5000);
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+          if (err.response.data.message) {
+            setErrorMessage(err.response.data.message);
+          } else {
+            setErrorMessage(
+              `Couldnt update ${newName}s number, please try again!`
+            );
+          }
+
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        });
+
+      setNewName("");
+      setPhoneNumber("");
+    }
+  };
+
+  return (
+    <>
+      <form className="ui form" onSubmit={onFormSubmit}>
+        <div className="field">
+          <label>Name</label>
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div className="field">
+          <label>Phonenumber</label>
+          <input
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </div>
+        <div>
+          <button className="ui button secondary" type="submit">
+            Add
+          </button>
+        </div>
+      </form>
+      <div className="ui hidden divider"></div>
+    </>
+  );
+};
+
+export default AddNewPersonForm;
---
+++ puhluetteloClient/src/components/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+
+import Notification from './Notification';
+import FilterNames from './FilterNames';
+import AddNewPersonForm from './AddNewPersonForm';
+import PhoneNumberList from './PhoneNumberList';
+import phoneService from '../services/PhoneNumbers';
+
+const App = () => {
+  const [phoneBook, setPhoneBook] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+
+  useEffect(() => {
+    phoneService
+      .getAll()
+      .then(numbers => {
+        setPhoneBook(numbers.data);
+      })
+      .catch(err => {
+        setErrorMessage(err.message);
+
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
+  }, []);
+
+  return (
+    <div className="ui container">
+      <h1 className="ui header">Phone😊</h1>
+
+      <FilterNames
+        placeholder="Filter names..."
+        filter={filter}
+        setFilter={setFilter}
+      />
+
+      <AddNewPersonForm
+        phoneBook={phoneBook}
+        setPhoneBook={setPhoneBook}
+        newName={newName}
+        setNewName={setNewName}
+        phoneNumber={phoneNumber}
+        setPhoneNumber={setPhoneNumber}
+        setErrorMessage={setErrorMessage}
+        setSuccessMessage={setSuccessMessage}
+      />
+      <Notification message={successMessage} errorMessage={errorMessage} />
+
+      <PhoneNumberList
+        title="Numbers"
+        filter={filter}
+        phoneBook={phoneBook}
+        setPhoneBook={setPhoneBook}
+        setSuccessMessage={setSuccessMessage}
+      />
+    </div>
+  );
+};
+
+export default App;
