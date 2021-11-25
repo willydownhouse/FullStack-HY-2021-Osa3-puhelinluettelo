69b8b68c97158601bb54e88c431f06b63734493c
willydownhouse <ville.l.alatalo@gmail.com>
2021-11-25 12:36:48 +0100

    first dev1

--- cypress/integration/phonebook.js
+++ cypress/integration/phonebook.js
@@ -3,16 +3,4 @@ describe('Phonebook app', function () {
     cy.visit('http://localhost:3001');
     cy.contains('PhoneBok');
   });
-
-  // it('you can add new name and number', function () {
-  //   cy.visit('http://localhost:3001');
-
-  //   cy.get('#nimi').type('willy');
-  //   cy.get('#number').type('12345678');
-
-  //   cy.get('#add').click();
-
-  //   cy.contains('willy');
-  //   cy.contains('12345678');
-  // });
 });
